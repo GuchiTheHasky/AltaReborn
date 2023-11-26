8d8b1e02c7197c1b541d1cb3abe847b32c02237e
GuchiTheHasky <guchithehasky@gmail.com>
2023-11-26 02:37:36 +0200

    initial commit

---
+++ src/main/java/com/alta/AltaRebornApplication.java
@@ -0,0 +1,13 @@
+package com.alta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AltaRebornApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AltaRebornApplication.class, args);
+	}
+
+}
