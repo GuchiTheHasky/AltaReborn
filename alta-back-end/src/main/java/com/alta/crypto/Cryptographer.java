1def6be3bad04ec5526525a5e4cc0da6b8f97c33
Nataliia <ruba.fish1500@gmail.com>
2023-12-20 12:37:52 +0200

    Change Cryptographer

--- alta-back-end/src/main/java/com/alta/crypto/Cryptographer.java
+++ alta-back-end/src/main/java/com/alta/crypto/Cryptographer.java
@@ -1,18 +1,20 @@
 package com.alta.crypto;
 
+import lombok.Value;
 import javax.crypto.Cipher;
 import javax.crypto.spec.SecretKeySpec;
 import java.nio.charset.StandardCharsets;
 import java.security.MessageDigest;
 import java.util.Arrays;
 
+@Value
 public class Cryptographer {
+    private static final String KEY = "encryptDecryptAesKey";
 
      /* Encrypt text: Cryptographer.crypt(Cipher.ENCRYPT_MODE, originalText);
         Decrypt text: Cryptographer.crypt(Cipher.DECRYPT_MODE, textEncrypted); */
 
     public static String crypt(int mode, String value) throws Exception {
-        String KEY = "encryptDecryptAesKey";
         byte[] valueBytes = value.getBytes(StandardCharsets.ISO_8859_1);
         byte[] keyBytes = KEY.getBytes(StandardCharsets.ISO_8859_1);
 
