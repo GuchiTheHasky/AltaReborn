3c9de9e52f438d9edc86a8e97ffb90d109806bfb
GuchiTheHasky <guchithehasky@gmail.com>
2024-01-04 16:53:40 +0200

    added server port 3000

--- alta-front-end/vite.config.ts
+++ alta-front-end/vite.config.ts
@@ -1,38 +1,10 @@
-// import { defineConfig } from 'vite';
-// import react from '@vitejs/plugin-react';
-//
-// // https://vitejs.dev/config/
-// export default defineConfig({
-// 	plugins: [react()]
-// });
-
-
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
-
-  resolve: {
-    alias: {
-      'next/navigation': 'react-router-dom',
-      'next/router': 'react-router-dom',
-    },
-  },
-
+	plugins: [react()],
   server: {
-    port: 3000,
-    proxy: {
-      "/api": {
-        target: "http://localhost:3001",
-        changeOrigin: true,
-        secure: false,
-        ws: true,
-      },
-    },
-  },
+      port: 3000,
+  }
 });
-
-
-
-
