3fc797838e2d1006c3b4cc6dd5b1613dc3766f0a
User <ruba.fish1500@gmail.com>
2024-01-08 17:34:04 +0200

    Edit V1.5_Alter_table_students_and_tasks.sql

--- alta-back-end/src/main/resources/db.migration/V1.5_Alter_table_students_and_tasks.sql
+++ alta-back-end/src/main/resources/db.migration/V1.5_Alter_table_students_and_tasks.sql
@@ -1,3 +1,2 @@
 alter table public.students drop column if exists tasks cascade;
-alter table public.task drop column if exists student_id cascade;
-alter table public.task drop column if exists student;
+
