7f7569f2d648327af777f0d6e2c31aa825c3f4d4
GuchiTheHasky <guchithehasky@gmail.com>
2024-01-17 18:49:50 +0200

    18:49

--- alta-back-end/src/main/java/com/alta/web/controller/TopicController.java
+++ alta-back-end/src/main/java/com/alta/web/controller/TopicController.java
@@ -2,6 +2,7 @@ package com.alta.web.controller;
 
 import com.alta.dto.TopicDto;
 import com.alta.facade.MainFacade;
+import com.alta.service.TopicService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -12,9 +13,11 @@ import java.util.List;
 @RestController
 public class TopicController {
     private final MainFacade mainFacade;
+    private final TopicService topicService;
 
     @GetMapping
     public List<TopicDto> findAll() {
-        return mainFacade.findAllTopics();
+//        return mainFacade.findAllTopics();
+        return topicService.findAll();
     }
 }
