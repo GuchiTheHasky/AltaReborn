d47b7c005d7e17ae664ed31d39d03f50c218fb24
GuchiTheHasky <guchithehasky@gmail.com>
2024-02-07 06:59:29 +0200

    added clerk

--- alta-front-end/src/main.tsx
+++ alta-front-end/src/main.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 import {QueryClient, QueryClientProvider} from '@tanstack/react-query';
+import {ClerkProvider} from '@clerk/clerk-react'
+
 
 const queryClient = new QueryClient();
+const PUBLISHABLE_KEY = import.meta.env.VITE_CLERK_PUBLISHABLE_KEY;
+if (!PUBLISHABLE_KEY) {
+    throw new Error("Missing Publishable Key")
+}
 
 async function enableMocking() {
     if (import.meta.env.VITE_API_MOCKED !== 'true') {
@@ -21,7 +27,9 @@ enableMocking().then(() => {
     ReactDOM.createRoot(document.getElementById('root')!).render(
         <React.StrictMode>
             <QueryClientProvider client={queryClient}>
-                <App/>
+                <ClerkProvider publishableKey={PUBLISHABLE_KEY}>
+                    <App />
+                </ClerkProvider>
             </QueryClientProvider>
         </React.StrictMode>
     );
