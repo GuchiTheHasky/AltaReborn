3ecc4fb8ec5cb27235febf32297e8c7daf0faec4
GuchiTheHasky <guchithehasky@gmail.com>
2024-02-21 19:03:40 +0200

    add documentation for tasks

--- alta-back-end/src/main/java/com/alta/web/controller/TaskController.java
+++ alta-back-end/src/main/java/com/alta/web/controller/TaskController.java
@@ -5,7 +5,6 @@ import com.alta.facade.MainFacade;
 import com.alta.web.entity.TaskResponse;
 import com.alta.web.entity.TaskRequest;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.ArraySchema;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
@@ -32,7 +31,9 @@ public class TaskController {
             description = "Successfully retrieved list of tasks.",
             content = @Content(
                     mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    array = @ArraySchema(schema = @Schema(implementation = TaskDto.class))))
+                    array = @ArraySchema(schema = @Schema(implementation = TaskDto.class))
+            )
+    )
     public List<TaskDto> findAll(
             @RequestParam(value = "studentIds") List<Integer> studentIds,
             @RequestParam(value = "topicIds") List<Integer> topicIds) {
@@ -40,6 +41,18 @@ public class TaskController {
     }
 
     @PostMapping("/assign")
+    @Operation(
+            summary = "Assign tasks for students.",
+            description = "Assign a list of tasks to specified students and returns the task group with current student.",
+            tags = "Task")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Tasks successfully assigned to students.",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(schema = @Schema(implementation = TaskResponse.class))
+            )
+    )
     public List<TaskResponse> receiveAssignmentTasks(@RequestBody TaskRequest request) {
         List<Integer> studentIds = request.studentsIds();
         List<Integer> tasksIds = request.tasksIds();
@@ -48,6 +61,18 @@ public class TaskController {
     }
 
     @PutMapping("/{id}")
+    @Operation(
+            summary = "Update a task.",
+            description = "Updates the topic, difficult level and answer of current task.",
+            tags = "Task")
+    @ApiResponse(
+            responseCode = "200",
+            description = "Task successfully updated.",
+            content = @Content(
+                    mediaType = MediaType.APPLICATION_JSON_VALUE,
+                    array = @ArraySchema(schema = @Schema(implementation = TaskDto.class))
+            )
+    )
     public TaskDto update(@PathVariable int id, @RequestBody TaskDto taskDto) {
         return mainFacade.updateTask(id, taskDto);
     }
