6978c55986a0bc388de4797c835f009ad3806165
Nataliia <ruba.fish1500@gmail.com>
2024-02-29 15:34:17 +0200

    change TopicController

--- alta-back-end/src/main/java/com/alta/web/controller/TopicController.java
+++ alta-back-end/src/main/java/com/alta/web/controller/TopicController.java
@@ -3,21 +3,16 @@ package com.alta.web.controller;
 import com.alta.dto.TopicDto;
 import com.alta.facade.MainFacade;
 import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.media.ArraySchema;
-import io.swagger.v3.oas.annotations.media.Content;
-import io.swagger.v3.oas.annotations.media.Schema;
-import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.constraints.Min;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/api/v1/topics")
 @RequiredArgsConstructor
@@ -27,44 +22,17 @@ public class TopicController {
 
     @GetMapping
     @Operation(
-            summary = "Get All Topics",
-            description = "Retrieves a list of all topics available in the system.",
+            summary = "Get all topics with optional pagination.",
             tags = "Topic")
-    @ApiResponse(
-            responseCode = "200",
-            description = "Successfully retrieved list of topics.",
-            content = @Content(
-                    mediaType = MediaType.APPLICATION_JSON_VALUE,
-                    array = @ArraySchema(schema = @Schema(implementation = TopicDto.class))))
-    public List<TopicDto> findAll() {
-        return mainFacade.findAllTopics();
+    public List<TopicDto> findAll(
+            @RequestParam(required = false, defaultValue = "0") @Min(0) Integer page,
+            @RequestParam(required = false, defaultValue = "10") @Min(1) Integer size) {
+
+        return Optional.ofNullable(page)
+                .flatMap(p -> Optional.ofNullable(size)
+                        .map(s -> PageRequest.of(page, size)))
+                .map(mainFacade::findAllTopicsPageByPage)
+                .orElseGet(mainFacade::findAllTopics);
     }
 
-
-    @GetMapping("/page")
-    @Operation(
-            summary = "Get page of topics.",
-            description = "Retrieves a page of topics.",
-            tags = "Topic")
-    @ApiResponses({
-            @ApiResponse(
-                    responseCode = "200",
-                    description = "Successfully retrieves a paginated list of topics.",
-                    content = @Content(
-                            mediaType = MediaType.APPLICATION_JSON_VALUE,
-                            array = @ArraySchema(schema = @Schema(implementation = TopicDto.class))
-                    )
-            ),
-            @ApiResponse(
-                    responseCode = "400",
-                    description = "Invalid pagination parameters."
-            )
-    })
-    public List<TopicDto> findAllTopicsPageByPage(
-            @RequestParam(defaultValue = "0") @Min(0) int page,
-            @RequestParam(defaultValue = "15") @Min(1) int size) {
-
-        PageRequest pageRequest = PageRequest.of(page, size);
-        return mainFacade.findAllTopicsPageByPage(pageRequest);
-    }
 }
